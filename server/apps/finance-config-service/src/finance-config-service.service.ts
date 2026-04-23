e835e7b0356bc0fba20696029e0f9336416afa05
JDRL555 <joshuleal3@gmail.com>
2026-04-23 17:50:55 -0400

    refactor: microservices structure to independient apps

--- server/apps/finance-config-service/src/finance-config-service.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class FinanceConfigServiceService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
--- server/apps/finance-config-service/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { FinanceConfigServiceModule } from './finance-config-service.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(FinanceConfigServiceModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
